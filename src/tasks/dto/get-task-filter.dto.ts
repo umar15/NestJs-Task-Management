7e78a2ee16c3aabf8106ee8f378dee2ad3f29333
Umar Khalid <umar.qur015@gmail.com>
2022-03-11 13:45:38 +0500

    nestjs task management

---
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -0,0 +1,11 @@
+import { TaskStatus } from '../task-status.enum';
+import { IsOptional, IsEnum, IsString } from 'class-validator';
+export class GetTaskFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TaskStatus } from './task-status.enum';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { TasksRepository } from './tasks.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { User } from 'src/auth/user.entity';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(TasksRepository)
+    private tasksRepository: TasksRepository,
+  ) {}
+
+  getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto, user);
+  }
+
+  createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    return this.tasksRepository.createTask(createTaskDto, user);
+  }
+
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const found = await this.tasksRepository.findOne({ where: { id, user } });
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
+  }
+
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
+    if (result.affected === 0) {
+      throw new NotFoundException();
+    }
+  }
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    let task = await this.getTaskById(id, user);
+    task.status = status;
+    await this.tasksRepository.save(task);
+    return task;
+  }
+}
